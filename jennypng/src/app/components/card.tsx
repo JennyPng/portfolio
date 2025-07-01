2d0c04952face94d06f9fe34711c881ac75239f4
Jenny Peng <jennypscale@gmail.com>
2025-06-30 22:41:58 -0700

    css improvements and data

--- jennypng/src/app/components/card.tsx
+++ jennypng/src/app/components/card.tsx
@@ -48,22 +48,22 @@ const formatText = (text: string | string[]) => {
 
 export default function Card({ title, description, image_paths, type_to_link, buttons, footer, className }: CardProps) {
   return (
-    <div className={`card border-2 my-4 mx-4 border-primary-green bg-color-background rounded-lg p-3 w-[70vw] md:min-w-[25vw] md:max-w-[300px] lg:w-[450px] grid gap-3 duration-170 hover:shadow-[-4px_3px_0px_0px] hover:shadow-primary-green ${className || ''}`}>
-      <div className="grid gap-2">
-        <p className="p-0 text-md">{title}</p>
+    <div className={`card flex-col border-2 my-4 mx-4 border-primary-green bg-color-background rounded-lg p-3 w-[70vw] md:min-w-[25vw] md:max-w-[300px] lg:w-[450px] duration-170 hover:shadow-[-4px_3px_0px_0px] hover:shadow-primary-green ${className || ''}`}>
+      <div className="flex-col">
+        <p className="pb-2 text-md">{title}</p>
         <hr className="border-1 -mx-3 border-primary-green" />
       </div>
       
-      <div className="grid gap-2 pt-4">
+      <div className="grid gap-2 pt-4 min-h-[200px] max-h-[600px]">
         {formatText(description)}
       </div>
       {
         buttons && (
-          <div className="flex-row">
+          <div className="flex-row pb-4">
             {
               buttons.map((button) => {
                 return(
-                  <button className="text-tertiary-green bg-primary-green justify min-w-fit p-2 mr-4 mt-2 flex-1 rounded-lg duration-170 hover:bg-secondary-green hover:text-primary hover:cursor-pointer" key={button.text}>{button.text}</button>
+                  <a href={button.link} target="_blank" rel="noopener noreferrer"><button className="text-tertiary-green bg-primary-green justify min-w-fit p-2 mr-4 mt-2 flex-1 rounded-lg duration-170 hover:bg-secondary-green hover:text-background hover:cursor-pointer" key={button.text}>{button.text}</button></a>
                 )
             })
             }
@@ -71,10 +71,10 @@ export default function Card({ title, description, image_paths, type_to_link, bu
         )
       }
       {footer && ( 
-        <> 
+        <div className="card-footer"> 
           <hr className="border-1 -mx-3 border-primary-green" />
           <p className="text-sm text-primary-green">{footer}</p>
-        </>
+        </div>
       )}
     </div>
   );
