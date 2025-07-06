4d60d8a185ca6217be3daa47dfde0cc6a32b68a3
Jenny Peng <jennypscale@gmail.com>
2025-07-05 23:54:02 -0700

    fix image-trigger

--- jennypng/src/app/about-me/page.tsx
+++ jennypng/src/app/about-me/page.tsx
@@ -1,7 +1,23 @@
 import Polaroid from "../components/polaroid"
+import Image from "next/image"
 
-function ImageTrigger() {
-
+function ImageTrigger({ text, imageSrc, altText } : { text: string, imageSrc: string, altText: string }) {
+    return (
+        <span className="relative inline-block group">
+            <span className="border-b-2 border-dotted border-gray-400 hover:border-gray-600 transition-colors duration-200 cursor-pointer">
+                {text}
+            </span>
+            <span className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-50 w-[500px]">
+                <Image 
+                    src={imageSrc} 
+                    alt={altText} 
+                    width={500}
+                    height={500}
+                    className="w-full h-[500px] rounded-sm shadow-lg object-cover"
+                />
+            </span>
+        </span>
+    )
 }
 
 export default function AboutMe() {
@@ -10,12 +26,12 @@ export default function AboutMe() {
             <Polaroid imageSrc="/images/grad.jpg" altText="picture of me" caption="I graduated from the University of Washington with a bachelors in CS 🥳" />
             <div className="flex-col flex-1 ml-8">
                 <h1>hi! i'm jenny, and</h1>
-                <p className="max-w-[50%] text-tertiary-green">
+                <p className="max-w-[70%] text-tertiary-green">
                     I have always loved to create, though the medium has shifted over the years.
                     <br></br>
                     <br></br>
 
-                    From coding warrior cat games on Scratch, to selling digital drawings on Deviantart, to playing violin in orchestras, to singing and writing songs, I have always found creating to be the most meaningful way to spend my time.
+                    From coding warrior cat games on Scratch, to selling digital drawings on Deviantart, to <ImageTrigger text="selling clothing with original art" imageSrc="/images/emo.gif" altText="image of clothing designed"></ImageTrigger>, to singing and writing songs, I have always found creating to be the most meaningful way to spend my time.
                     <br></br>
                     <br></br>
 
--- jennypng/src/app/components/animated-blobs.tsx
+++ jennypng/src/app/components/animated-blobs.tsx
@@ -6,7 +6,7 @@ export default function AnimatedBlobsBackground() {
   return (
     <div className="absolute inset-0 pointer-events-none z-0">
       <motion.div
-        className="absolute w-96 h-80 bg-[radial-gradient(circle_at_60%_40%,var(--color-teal)_40%,var(--color-primary-pink)_10%)] rounded-full blur-3xl"
+        className="z-0 absolute w-96 h-80 bg-[radial-gradient(circle_at_60%_40%,var(--color-teal)_40%,var(--color-primary-pink)_10%)] rounded-full blur-3xl"
         animate={{
           x: [0, -120, 80, -100, 0],
           y: [0, 120, -80, 100, 0],
@@ -18,10 +18,10 @@ export default function AnimatedBlobsBackground() {
           ease: "easeInOut",
           delay: 5,
         }}
-        style={{ top: "50%", right: "10%" }}
+        style={{ top: "50%", right: "5%" }}
       />
       <motion.div
-        className="absolute w-96 h-80 bg-[radial-gradient(circle_at_60%_40%,var(--color-teal)_60%,var(--color-primary-pink)_10%)] rounded-full blur-3xl"
+        className="z-0 absolute w-96 h-80 bg-[radial-gradient(circle_at_60%_40%,var(--color-teal)_60%,var(--color-primary-pink)_10%)] rounded-full blur-3xl"
         animate={{
           x: [0, 80, -120, 60, 0],
           y: [0, -80, 120, -60, 0],
@@ -33,7 +33,7 @@ export default function AnimatedBlobsBackground() {
           ease: "easeInOut",
           delay: 10,
         }}
-        style={{ bottom: "20%", left: "20%" }}
+        style={{ top: "10%", left: "10%" }}
       />
     </div>
   );
