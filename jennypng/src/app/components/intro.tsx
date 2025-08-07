f7c84b048cd53c435e57a25cee75f2bbd9745a61
Jenny Peng <jennypscale@gmail.com>
2025-08-06 23:11:44 -0700

    portrait

--- jennypng/src/app/components/intro.tsx
+++ jennypng/src/app/components/intro.tsx
@@ -1,13 +1,29 @@
+'use client'
 import Card from "./card";
 import cardsData from "../data/cards.json";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Intro({className} : {className?: string}) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="relative min-h-[400px] flex items-center justify-center">
-      <div className="relative z-10 w-full flex items-center justify-center">
+    <div className="relative min-h-[400px] flex flex-col md:flex-row items-center md:-mt-10">
+        <div 
+          className="relative w-[100%] md:w-[1200px]"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)} 
+        >
+          <Image
+            src={isHovered ? "/images/2025me2.png" : "/images/2025me1.png"}
+            alt="Jenny"
+            width={1200}
+            height={1500}
+            className="object-cover transition-opacity mx-0 duration-300"
+          />
+        </div>
         <Card {...cardsData.intro} maxWidth={400} className={`${className}`} />
       </div>
-    </div>
   );
 }
  
--- jennypng/src/app/page.tsx
+++ jennypng/src/app/page.tsx
@@ -6,10 +6,10 @@ import AnimatedBlobsBackground from "./components/animated-blobs";
 export default function Home() {
 
   return (  
-    <div className="items-center justify-center min-h-screen p-4 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-center min-h-screen pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <AnimatedBlobsBackground />
-      <Intro className="inline-block ml-4 md:ml-[30vw]" />
-      <Projects className="mt-[15vh] ml-4"/>
+      <Intro className="inline-block ml-4" />
+      <Projects className="mt-[12vh] ml-4"/>
       <Experience className="mt-[15vh] ml-4"/>
     </div>
   );
